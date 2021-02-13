7a4c8496d14bb0352b0dc0185a94785fa41a5020
keiichi.m <k.thienran@gmail.com>
2021-02-13 13:02:12 +0900

    test7

--- image_translator.py
+++ image_translator.py
@@ -1,33 +1,77 @@
-from PIL import Image
-import sys
-sys.path.append('/path/to/dir')
+import requests
 
-import pyocr
-import pyocr.builders
-from googletrans import Translator
+KEY1 = os.environ["COMPUTER_VISION_API_KEY1"]
 
-def translate_eng_image_to_ja(image=None):
+endpoint = 'https://eastasia.api.cognitive.microsoft.com/vision/v1.0/ocr'
+
+def translate_eng_image_to_ja(image_url=None, image=None):
     print("translate_eng_image_to_ja : in")
 
-    if image is None:
+    if image_url is None and image is None:
         return '必要な情報が足りません'
 
-    tools = pyocr.get_available_tools()
-    if len(tools) == 0:
-        print("No OCR tool found")
-        sys.exit(1)
-    tool = tools[0]
-    #print("Will use tool '%s'" % (tool.get_name()))
+    params = {'visualFeatures': 'Categories,Description,Color'}
 
-    langs = tool.get_available_languages()
-    #print("Available languages: %s" % ", ".join(langs))
+    if image_url:
+        headers = {
+            'Ocp-Apim-Subscription-Key': KEY1,
+            'Content-Type': 'application/json',
+        }
+        data = {'url': image_url}
+        response = requests.post(
+            endpoint,
+            headers=headers,
+            params=params,
+            json=data
+        )
 
-    txt = tool.image_to_string(
-        Image.open(image),
-        lang='eng',
-        builder=pyocr.builders.TextBuilder()
-    )
- 
-    translator = Translator()
-    translated = translator.translate(txt, dest="ja")
-    return translated.text
+    elif image is not None:
+        headers = {
+            'Ocp-Apim-Subscription-Key': KEY1,
+            "Content-Type": "application/octet-stream"
+        }
+        response = requests.post(
+            endpoint,
+            headers=headers,
+            params=params,
+            data=image,
+        )
+
+    status = response.status_code
+    data = response.json()
+
+    if status != 200:
+
+        if data['code'] == 'InvalidImageSize':
+            text = '画像のサイズが大きすぎます'
+
+        elif data['code'] == 'InvalidImageUrl':
+            text = 'この画像URLからは取得できません'
+
+        elif data['code'] == 'InvalidImageFormat':
+            text = '対応していない画像形式です'
+
+        else:
+            text = 'エラーが発生しました'
+
+        print(status, data)
+        return text
+
+    text = ''
+    for region in data['regions']:
+        for line in region['lines']:
+            for word in line['words']:
+                text += word.get('text', '')
+                if data['language'] != 'en':
+                    text += ' '
+        text += '\n'
+
+    if len(text) == 0:
+        text += '文字が検出できませんでした'
+
+    print('text:', text)
+    return text
+
+
+if __name__ == "__main__":
+    get_text_by_ms(image_url)
