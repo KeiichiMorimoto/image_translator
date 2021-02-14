06d07a7d821f81e81ccc3a53b98d7c08c345b2fe
keiichi.m <k.thienran@gmail.com>
2021-02-14 14:07:58 +0900

    test

--- main.py
+++ main.py
@@ -88,14 +88,14 @@ def handle_image(event):
   #message_id = event.message.id
   #image_url = getImageLine(message_id)
   message_content = line_bot_api.get_message_content(event.message.id)
-
-　with open("static/" + event.message.id + ".jpg", "wb") as f:
+  
+  with open('static/' + event.message.id + '.jpg', 'wb') as f:
     f.write(message_content.content)
     line_bot_api.reply_message(
       event.reply_token,
       ImageSendMessage(
-        original_content_url=FQDN + "/static/" + event.message.id + ".jpg",
-        preview_image_url=FQDN + "/static/" + event.message.id + "jpg"
+        original_content_url = FQDN + '/static/' + event.message.id + '.jpg',
+        preview_image_url = FQDN + '/static/' + event.message.id + 'jpg'
       )
     )
 
